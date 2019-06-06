cf846f6bacd32bda72fb8c019930df4548b9784b
Pinkie Aruwani <aruwanip@gmail.com>
2019-06-06 02:41:00 -0400

    Fix public quiz

--- src/views/PublicHome.vue
+++ src/views/PublicHome.vue
@@ -1,9 +1,11 @@
 <template>
-  <v-container class="text-xs-center">
-    <div id="takeQuiz" @click="navigate({url: '/PublicQuiz'})">
+  <v-container id="publicHomeContainer" class="text-xs-center">
+    <div id="takeQuiz" @click="nextPage()">
       <v-img height="50%" width="50%" :src="require('@/assets/zeus.png')"></v-img>
-      <h2>Hello!</h2>
-      <h2>Thank you for your interest in Personal Finance</h2>
+      <v-content id="publicMessages" class="salsbury" v-html="messages[index]">
+        <!-- <h2>Hello!</h2>
+        <h2>Thank you for your interest in Personal Finance</h2> -->
+      </v-content>
     </div>
   </v-container>
 </template>
@@ -16,8 +18,24 @@ export default {
   created() {
     store.commit("navigation/setTopBarTitle", "Public");
   },
+  data() {
+    return {
+      index: 0,
+      messages: [
+        'Hello!<br>Thank you for your interest in financial literacy.',
+        'Did you know that 30% of millenials have overdrawn their bank account in the last year and 48% say that they probably or definitely could not come up with $2000 for an unexpected expense?',
+        'Well, financial literacy has not been our greatest of skills. Here is where Wise and their nifty mobile app could help you. Let us start with a few questions...'
+      ]
+    }
+  },
   methods: {
-    ...mapActions("navigation", ["navigate"])
+    ...mapActions("navigation", ["navigate"]),
+    nextPage() {
+      this.index++
+      if (this.index > 2) {
+        this.navigate({url: '/quizzes/multiplechoicepublic'})
+      }
+    }
   }
 };
 </script>
@@ -26,14 +44,22 @@ export default {
 #container {
   display: block;
 }
+#publicHomeContainer {
+  display: block;
+}
+#publicMessages {
+  font-size: 30px;
+  justify-content: center;
+  padding: 0 35px !important;
+}
 #takeQuiz {
   text-align: center;
 }
 #takeQuiz > div {
   margin-left: auto;
   margin-right: auto;
-  margin-top: 25px;
-  margin-bottom: 50px;
+  margin-top: 30px;
+  margin-bottom: 30px;
 }
 #takeQuiz > h2 {
   font-style: italic;
