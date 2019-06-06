274e0f71004f3fdc7ef2c5ad304c7a13f5bab5aa
janethjepkogei <janethjepkogei>
2019-06-06 00:02:20 -0400

    hardcoded categories

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -1,121 +1,136 @@
 <template>
   <v-container id="categoriesContainer" class="container text-xs-center">
-      <h1 id="pickCategory" class="salsbury">Pick a Category</h1>
-      <v-list v-for="(i, index) in categoryNames" :key="i.id" class="accent" id="list">
-        <v-list-title avatar style="display: flex;">
-          <v-list-tile-avatar class="avatar">
-            <img :src="categoryImages[index].avatar"/>
-          </v-list-tile-avatar>
-          <v-list-tile-content class="salsbury secondary">
-            {{i.name}}
-          </v-list-tile-content>
-          <v-list-tile-avatar class="secondary">
-            <v-icon :color="categoryImages[index].status">{{icons[categoryImages[index].status]}}</v-icon>
-          </v-list-tile-avatar>
-        </v-list-title>
-      </v-list>
-      <div id="more" class="salsbury">More ></div>
-      <mentor-advice title="A Message from Plato" :message="message"></mentor-advice>
-      <!-- <v-img id="mentor" :src="require('@/assets/user.png')"></v-img> -->
+    <h1 id="pickCategory" class="salsbury">Pick a Category</h1>
+    <v-list v-for="(i, index) in categoryNames" :key="i.id" class="accent" id="list">
+      <v-list-title avatar style="display: flex;">
+        <v-list-tile-avatar class="avatar">
+          <img :src="categoryImages[index].avatar">
+        </v-list-tile-avatar>
+        <v-list-tile-content class="salsbury secondary">{{i.name}}</v-list-tile-content>
+        <v-list-tile-avatar class="secondary">
+          <v-icon :color="categoryImages[index].status">{{icons[categoryImages[index].status]}}</v-icon>
+        </v-list-tile-avatar>
+      </v-list-title>
+    </v-list>
+    <div id="more" class="salsbury">More ></div>
+    <mentor-advice title="A Message from Plato" :message="message"></mentor-advice>
+    <!-- <v-img id="mentor" :src="require('@/assets/user.png')"></v-img> -->
   </v-container>
 </template>
 
 <script>
-import store from '@/store'
-import MentorAdvice from '@/components/MentorAdvice.vue'
-import {quizService} from '@/services/quiz.service'
+import store from "@/store";
+import MentorAdvice from "@/components/MentorAdvice.vue";
+import { quizService } from "@/services/quiz.service";
 export default {
   components: {
     MentorAdvice
   },
   created() {
-    store.commit('navigation/setTopBarTitle', 'Quiz Categories');
-    quizService.getQuizCategories().then(
-      res => {
-        this.categoryNames = res.data;
-      }
-    );
+    store.commit("navigation/setTopBarTitle", "Quiz Categories");
+    quizService.getQuizCategories().then(res => {
+      this.categoryNames = res.data;
+    });
   },
-  data () {
+  data() {
     return {
-      categoryNames: [],
+      categoryNames: [
+        {
+          name: "Goal Setting"
+        },
+        {
+          name: "Spending"
+        },
+        {
+          name: "Taxation"
+        },
+        {
+          name: "Cost of College"
+        },
+        {
+          name: "Charitable Giving"
+        },
+        {
+          name: "GENERAL"
+        }
+      ],
       categoryImages: [
         {
-          avatar: require('@/assets/target.png'),
-          status: 'green'
+          avatar: require("@/assets/target.png"),
+          status: "green"
         },
         {
-          avatar: require('@/assets/spending.png'),
-          status: 'red'
+          avatar: require("@/assets/spending.png"),
+          status: "red"
         },
         {
-          avatar: require('@/assets/tax.png'),
-          status: 'amber'
+          avatar: require("@/assets/tax.png"),
+          status: "amber"
         },
         {
-          avatar: require('@/assets/education.png'),
-          status: 'green'
+          avatar: require("@/assets/education.png"),
+          status: "green"
         },
         {
-          avatar: require('@/assets/heart.png'),
-          status: 'red'
+          avatar: require("@/assets/heart.png"),
+          status: "red"
         },
         {
-          avatar: require('@/assets/shield.png'),
-          status: 'amber'
+          avatar: require("@/assets/shield.png"),
+          status: "amber"
         }
       ],
       icons: {
-        red: 'error',
-        amber: 'warning',
-        green: 'check_circle'
+        red: "error",
+        amber: "warning",
+        green: "check_circle"
       },
-      message: 'Hi Eddy,<br><br>' +
-      'You may need to focus on Spending. You have taken fewer (0) quizzes this topic! ' + 
-      'You have also identified Spending as a weak area.'
-    }
+      message:
+        "Hi Eddy,<br><br>" +
+        "You may need to focus on Spending. You have taken fewer (0) quizzes this topic! " +
+        "You have also identified Spending as a weak area."
+    };
   },
-  methods: {
-  }
-}
+  methods: {}
+};
 </script>
 
 <style>
-  v-list-title > div.v-list__tile__avatar.avatar {
-    justify-content: center;
-  }
-  #categoriesContainer {
-    padding-left: 20px;
-    padding-right: 20px;
-  }
-  #list {
-    margin-bottom: -10px;
-    padding: 3px 3px 3px 0;
-  }
-  #list > v-list-title > div.v-list__tile__content {
-    padding: 0 8px;
-  }
-  #list > v-list-title > div.avatar {
-    align-items: center;
-  }
-  #list > v-list-title > div.secondary {
-    align-items: center;
-    font-size: 20px;
-    font-style: oblique;
-    height: 40px;
-    text-align: left;
-  }
-  #more {
-    color: #4F8FCF;
-    font-size: 15px;
-    font-style: oblique;
-    margin-bottom: -10px;
-    margin-top: -10px;
-    text-align: right;
-  }
-  #pickCategory {
-    color: #4F8FCF;
-    font-style: oblique;
-    font-size: 25px;
-  }
+v-list-title > div.v-list__tile__avatar.avatar {
+  justify-content: center;
+}
+#categoriesContainer {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+#list {
+  margin-bottom: -10px;
+  padding: 3px 3px 3px 0;
+}
+#list > v-list-title > div.v-list__tile__content {
+  padding: 0 8px;
+}
+#list > v-list-title > div.avatar {
+  align-items: center;
+}
+#list > v-list-title > div.secondary {
+  align-items: center;
+  font-size: 20px;
+  font-style: oblique;
+  height: 40px;
+  text-align: left;
+}
+#more {
+  color: #4f8fcf;
+  font-size: 15px;
+  font-style: oblique;
+  margin-bottom: -10px;
+  margin-top: -10px;
+  text-align: right;
+}
+#pickCategory {
+  color: #4f8fcf;
+  font-style: oblique;
+  font-size: 25px;
+}
 </style>
