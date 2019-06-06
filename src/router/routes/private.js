c835a5c69e4800755e45a6e674df3ac3451bcaf6
Joseph <joseprem@gmail.com>
2019-06-06 00:26:45 -0400

    added qa-forum

--- src/router/routes/private.js
+++ src/router/routes/private.js
@@ -14,6 +14,7 @@ import PublicResult from "@/views/PublicResult.vue";
 import QuizResult from '@/views/QuizResult.vue';
 import StudentQuizzes from "@/views/StudentQuizzes.vue";
 import Resources from "@/views/Resources.vue";
+import QAForum from "@/views/qa-forum.vue"
 
 const routes = [
   {
@@ -95,6 +96,11 @@ const routes = [
     path: "/resources",
     name: "resources",
     component: Resources
+  },
+  {
+    path: "/QAForum",
+    name: "QAForum",
+    component: QAForum
   }
 ];
 
---
+++ src/views/qa-forum.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-container>
+        <div class="mentor">
+            <v-img height="20%" width="20%" :src="require('@/assets/conversation.png')"></v-img>
+            <!-- <h2>ASK</h2> -->
+            <p></p>
+            <v-img height="100%" width="75%" :src="require('@/assets/Ask-your-question-here.png')"></v-img>
+            <p></p>
+            <v-img height="100%" width="95%" :src="require('@/assets/QA-panel.png')"></v-img>
+        </div>        
+        
+    </v-container>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import store from '@/store'
+
+export default {
+    components: {
+    },
+    created() {
+        store.commit('navigation/setTopBarTitle', 'QAForum')
+    },
+    methods: {
+        ...mapActions('navigation', ['navigate'])
+    }
+}
+</script>
+
+<style>
+    .button {
+        height: 30%;
+        text-align: center;
+        width: 30%;
+    }
+    #actions {
+        display: flex;
+        justify-content: space-around;
+        margin-top: 60px;
+    }
+    #status {
+        color: #4F8FCF;
+        font-size: 20px;
+        font-style: oblique;
+        font-weight: bold;
+        text-align: center;
+    }
+    .mentor {
+        text-align: center;
+    }
+    .mentor > div {
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 25px;
+    }
+    .mentor > h2 {
+        font-family: 'Times New Roman', Times, serif;
+        font-size: 45px;
+    }
+</style>
