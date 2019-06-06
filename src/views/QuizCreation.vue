ddd834b0d8e86bb3500936b7bdcbaa65ca879d69
Zebulon Morgan <zebmorgan@gmail.com>
2019-06-05 22:13:53 -0400

    Fix QuizCreation

--- src/views/QuizCreation.vue
+++ src/views/QuizCreation.vue
@@ -1,6 +1,7 @@
 <template>
-	<v-container class="container">
-		<v-img style="margin-left: auto;" :src="require('@/assets/exam.png')"></v-img>
+	<v-container >
+		<div style="margin-left: 10px; margin-right: 10px;">
+		<v-img style="margin-left: auto; margin-top:" height="100%" width="15%" :src="require('@/assets/exam.png')"></v-img>
 		<v-flex xs12>
           <v-text-field
             label="Quiz Name"
@@ -28,6 +29,7 @@
   		<v-btn style="margin-left: 0px;" color="info">Upload Pictures</v-btn>
   		<v-img style="margin-left: 20px; float: right; margin-top: 100px;" height="15%" width="15%" :src="require('@/assets/checked.png')"></v-img>
   		<v-img style="margin-left: auto; float: right; margin-top: 100px;" height="15%" width="15%" :src="require('@/assets/cancel.png')"></v-img>
+  	</div>
 	</v-container>
 </template>
 
